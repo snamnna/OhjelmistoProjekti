2a54a4286995b11c55715efeb37587253ba9d0b4
matiasdn <matiasdn@metropolia.fi>
2023-02-03 23:13:42 +0200

    formatting

--- simulaattorinrunko5/src/simu/framework/Saapumisprosessi.java
+++ simulaattorinrunko5/src/simu/framework/Saapumisprosessi.java
@@ -1,21 +1,23 @@
 package simu.framework;
+
 import eduni.distributions.*;
 import simu.model.TapahtumanTyyppi;
+
 public class Saapumisprosessi {
-	
-	private ContinuousGenerator generaattori;
-	private Tapahtumalista tapahtumalista;
-	private TapahtumanTyyppi tyyppi;
 
-	public Saapumisprosessi(ContinuousGenerator g, Tapahtumalista tl, TapahtumanTyyppi tyyppi){
-		this.generaattori = g;
-		this.tapahtumalista = tl;
-		this.tyyppi = tyyppi;
-	}
+    private ContinuousGenerator generaattori;
+    private Tapahtumalista tapahtumalista;
+    private TapahtumanTyyppi tyyppi;
 
-	public void generoiSeuraava(){
-		Tapahtuma t = new Tapahtuma(tyyppi, Kello.getInstance().getAika()+generaattori.sample());
-		tapahtumalista.lisaa(t);
-	}
+    public Saapumisprosessi(ContinuousGenerator g, Tapahtumalista tl, TapahtumanTyyppi tyyppi) {
+        this.generaattori = g;
+        this.tapahtumalista = tl;
+        this.tyyppi = tyyppi;
+    }
+
+    public void generoiSeuraava() {
+        Tapahtuma t = new Tapahtuma(tyyppi, Kello.getInstance().getAika() + generaattori.sample());
+        tapahtumalista.lisaa(t);
+    }
 
 }
