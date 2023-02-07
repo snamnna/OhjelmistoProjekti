b5c0776a46d3795159092f59e730ed63c5c06d60
matiasdn <matiasdn@metropolia.fi>
2023-02-07 15:29:14 +0200

    switch fix

--- simulaattorinrunko5/src/simu/model/util/FPalvelupiste.java
+++ simulaattorinrunko5/src/simu/model/util/FPalvelupiste.java
@@ -6,12 +6,18 @@ import simu.model.*;
 
 public class FPalvelupiste {
     public static IPalvelupiste createPalvelupiste(TapahtumanTyyppi tyyppi, Tapahtumalista tapahtumalista) {
-        return switch (tyyppi) {
-            case ARR: yield new Sairaanhoitaja(new Normal(10, 6), tapahtumalista, TapahtumanTyyppi.ARR);
-            case YLARR: yield new YLaakari(new Normal(10, 10), tapahtumalista, TapahtumanTyyppi.YLARR);
-            case ELARR: yield new ELaakari(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.ELARR);
-            case LABRA_ARRIVAL: yield new Labra(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.LABRA_ARRIVAL);
+        IPalvelupiste uusiPalvelupiste;
+        switch (tyyppi) {
+            case ARR: uusiPalvelupiste = new Sairaanhoitaja(new Normal(10, 6), tapahtumalista, TapahtumanTyyppi.ARR);
+                break;
+            case YLARR: uusiPalvelupiste = new YLaakari(new Normal(10, 10), tapahtumalista, TapahtumanTyyppi.YLARR);
+                break;
+            case ELARR: uusiPalvelupiste = new ELaakari(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.ELARR);
+                break;
+            case LABRA_ARRIVAL: uusiPalvelupiste = new Labra(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.LABRA_ARRIVAL);
+                break;
             default: throw new IllegalArgumentException("Unexpected value: " + tyyppi);
         };
+        return uusiPalvelupiste;
     }
 }
