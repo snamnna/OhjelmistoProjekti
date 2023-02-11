b1b2d0a1b747cd36a3e51ab9efbc7291c6456af4
matiasdn <matiasdn@metropolia.fi>
2023-02-11 15:50:51 +0200

    kontrollerille hakemisto ja tiedostot

---
+++ src/controller/IKontrolleri.java
@@ -0,0 +1,8 @@
+package controller;
+
+public interface IKontrolleri {
+		// Rajapinta, joka tarjotaan  käyttöliittymälle:
+		
+		// Rajapinta, joka tarjotaan moottorille:
+
+}
---
+++ src/controller/Kontrolleri.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javafx.application.Platform;
+import simu.framework.IMoottori;
+import simu.model.OmaMoottori;
+import view.ISimulaattorinUI;
+
+public class Kontrolleri implements IKontrolleri {   // UUSI
+
+}
