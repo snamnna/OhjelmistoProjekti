e8833b8fe861cf441d18f18b4f0c95a1b670cfce
edvni <edvard.nivala@metropolia.fi>
2023-02-12 19:22:20 +0200

    Kontrolleriin jotain

--- src/controller/Kontrolleri.java
+++ src/controller/Kontrolleri.java
@@ -7,4 +7,22 @@ import view.ISimulaattorinUI;
 
 public class Kontrolleri implements IKontrolleri {   // UUSI
 
-}
+	private ISimulaattorinUI ui;
+	
+	public Kontrolleri(ISimulaattorinUI ui) {
+		this.ui = ui;
+		// lisää?
+	}
+	
+	public void naytaLoppuaika(double aika) {
+		// Platform.runLater(() -> ui.setLoppuaika(aika));
+	}
+	
+	public void visualisoiAsiakas(){
+		/* Platform.runLater(new Runnable() {
+			public void run() {
+				 ui.getVisualisointi.uusiAsiakas();
+			}
+		}); */
+	}
+ }
