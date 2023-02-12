3e2b6f1bc22eddcfe3e86f6031016adad451526f
edvni <edvard.nivala@metropolia.fi>
2023-02-12 19:33:02 +0200

    ISimuUI jutut

--- src/view/ISimulaattorinUI.java
+++ src/view/ISimulaattorinUI.java
@@ -2,4 +2,14 @@ package view;
 
 public interface ISimulaattorinUI {
 
+	// Kontrolleri tarvitsee syötteitä, jotka se välittää Moottorille
+		public double getAika();
+		public long getViive();
+		
+		//Kontrolleri antaa käyttöliittymälle tuloksia, joita Moottori tuottaa 
+		public void setLoppuaika(double aika);
+		
+		// Kontrolleri tarvitsee  
+		public IVisualisointi getVisualisointi();
+
 }
