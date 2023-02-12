937de10e64ad09a60418950ced87cb648e410f36
edvni <edvard.nivala@metropolia.fi>
2023-02-12 18:51:45 +0200

    Appi pohja

--- src/view/SimulaattorinGUI.java
+++ src/view/SimulaattorinGUI.java
@@ -5,6 +5,8 @@ import java.text.DecimalFormat;
 import controller.*;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -23,4 +25,25 @@ import javafx.scene.text.*;
 
 public class SimulaattorinGUI extends Application implements ISimulaattorinUI{
 
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			init();
+			primaryStage.setTitle("Päivystyssimulaattori");
+			
+			HBox hBox = new HBox();
+			
+			Scene scene = new Scene(hBox, 1024, 768);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
