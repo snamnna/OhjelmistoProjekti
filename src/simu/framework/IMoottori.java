bf1d37334bdacabb4f5dc43b5271156f151d6b9d
tuiskuna <tuiskunarhi@gmail.com>
2023-02-13 09:55:56 +0200

    Imoottoriin lisätty pari metodia
    Lisätty metodit lääkärien/palvelipisteiden määrien asettamiseksi.

--- src/simu/framework/IMoottori.java
+++ src/simu/framework/IMoottori.java
@@ -3,7 +3,8 @@ package simu.framework;
 public interface IMoottori {
 	// Kontrolleri käyttää tätä rajapintaa
 	public void setSimulointiaika(double aika);
-	public double setLabJakauma(); //annetaan asetettava jakauma
-
+	public double setLabJakauma(); //asetettava jakauma
+	public void setYlaakarienLkm(); //asetettavat palvelupisteet
+	public void setElaakarienLkm();
 	//tarvitaanko muita?
 }
