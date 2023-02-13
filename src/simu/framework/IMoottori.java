845b6f049b14b5b29c47c98e0898b0b6fbfb1e1f
tuiskuna <tuiskunarhi@gmail.com>
2023-02-13 09:50:57 +0200

    Imoottori luotu ja lisätty muutama metodi
    Luotu uusi interface moottorille ja lisätty sinne metodeja

---
+++ src/simu/framework/IMoottori.java
@@ -0,0 +1,9 @@
+package simu.framework;
+
+public interface IMoottori {
+	// Kontrolleri käyttää tätä rajapintaa
+	public void setSimulointiaika(double aika);
+	public double setLabJakauma(); //annetaan asetettava jakauma
+
+	//tarvitaanko muita?
+}
