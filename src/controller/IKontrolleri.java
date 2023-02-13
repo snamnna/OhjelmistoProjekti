9117a807eb378784340bafa247477b0e2c75be30
tuiskuna <tuiskunarhi@gmail.com>
2023-02-13 09:48:28 +0200

    Ikontrolleria täydennetty
    Ikontrolleriin lisätty tarvittavia metodeja, lisäillään ja muokataan tarvittaessa

--- src/controller/IKontrolleri.java
+++ src/controller/IKontrolleri.java
@@ -1,8 +1,16 @@
 package controller;
 
 public interface IKontrolleri {
-		// Rajapinta, joka tarjotaan  käyttöliittymälle:
-		
-		// Rajapinta, joka tarjotaan moottorille:
+	
+	// Rajapinta, joka tarjotaan  käyttöliittymälle:
+	public void naytaLoppuaika(double aika);
+	public void visualisoiAsiakas();
+	
+	// Rajapinta, joka tarjotaan moottorille:
+	public void kaynnistaSimulointi();
+	public int naytaYlaakarienLkm();
+	public int naytaElaakarienLkm();
+	public double naytaLabJakauma();
 
+	//muita? (onko ees oikeita :D)
 }
