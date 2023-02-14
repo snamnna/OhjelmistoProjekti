6b0f2a97856d2f42e1468c9fbd638a87a92471d3
Matias <matiasdn@metropolia.fi>
2023-02-14 15:31:54 +0200

    metodien päivitys

--- src/view/ISimulaattorinUI.java
+++ src/view/ISimulaattorinUI.java
@@ -2,17 +2,19 @@ package view;
 
 public interface ISimulaattorinUI {
 
-	//
 	public int getYlaakarienLkm();
+
 	public int getElaakarienLkm();
+
 	public double getLabraJakauma();
-	public double getSimulointiAika(); //simuloinnin kesto kontrollerille
 
-	//Kontrolleri antaa käyttöliittymälle tuloksia, joita Moottori tuottaa 
+	public double getSimulointiAika(); // simuloinnin kesto kontrollerille
+
+	// Kontrolleri antaa käyttöliittymälle tuloksia, joita Moottori tuottaa
 	public void setLoppuaika(double aika);
-	public void setTulokset(); //asetetaan tulokset näkyville, tähän pitää selvittää mitä tyyppiä jne
 
+	public void setTulokset(); // asetetaan tulokset näkyville, tähän pitää selvittää mitä tyyppiä jne
 
-	// Kontrolleri tarvitsee  
+	// Kontrolleri tarvitsee
 	public IVisualisointi getVisualisointi();
-	}
+}
