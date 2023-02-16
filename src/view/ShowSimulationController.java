479f2335e5de2b61775eed36693675962ccda3dc
snamnna <sanna.lohkovuori@hotmail.com>
2023-02-16 15:37:51 +0200

    Muokattu controlleria ja kayttöliittymän fxml tiedostoa

--- src/view/ShowSimulationController.java
+++ src/view/ShowSimulationController.java
@@ -1,10 +1,21 @@
 package view;
 
+import javafx.fxml.FXML;
+import simu.model.OmaMoottori;
+
 public class ShowSimulationController {
 	
 	private TESTISimulaationGUI mainapp;
+	
+	private OmaMoottori moottori = new OmaMoottori();
+	
 	//Referoi itsensä, muokataan oikeaksi myöhemmin
-		public void setTesti(TESTISimulaationGUI testiSimulaationGUI) {
-			this.mainapp = testiSimulaationGUI;
-		}
+	public void setTesti(TESTISimulaationGUI testiSimulaationGUI) {
+		this.mainapp = testiSimulaationGUI;
+	}
+
+	@FXML
+	private void handleAloitaSimulointi() {
+		moottori.aja(); //Tällä aloitetaan simulointi
+	}
 }
