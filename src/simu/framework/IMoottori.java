f529cb3342529153fbbeabd26a47cb82afd2cf87
Matias <matiasdn@metropolia.fi>
2023-02-17 23:03:10 +0200

    muutettu setLabJakauman return typeksi void

--- src/simu/framework/IMoottori.java
+++ src/simu/framework/IMoottori.java
@@ -3,7 +3,7 @@ package simu.framework;
 public interface IMoottori {
 	// Kontrolleri käyttää tätä rajapintaa
 	public void setSimulointiaika(double aika);
-	public double setLabJakauma(); //asetettava jakauma
+	public void setLabJakauma(); //asetettava jakauma
 	public void setYlaakarienLkm(); //asetettavat palvelupisteet
 	public void setElaakarienLkm();
 	//tarvitaanko muita?
