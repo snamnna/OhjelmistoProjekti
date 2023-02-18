b712ad56c3ebd129c91345526173e85c50b0e29b
matiasdn <matiasdn@metropolia.fi>
2023-02-18 21:04:17 +0200

    muuttuja tapahtuman luovan palvelupisteen id:lle

--- src/simu/framework/Tapahtuma.java
+++ src/simu/framework/Tapahtuma.java
@@ -3,33 +3,47 @@ package simu.framework;
 import simu.model.TapahtumanTyyppi;
 
 public class Tapahtuma implements Comparable<Tapahtuma> {
-	
-		
+
 	private TapahtumanTyyppi tyyppi;
 	private double aika;
-	
-	public Tapahtuma(TapahtumanTyyppi tyyppi, double aika){
+	private int palvelupisteID;
+
+	public Tapahtuma(TapahtumanTyyppi tyyppi, double aika, int palvelupisteID) {
 		this.tyyppi = tyyppi;
 		this.aika = aika;
+		this.palvelupisteID = palvelupisteID;
 	}
-	
+
+	public void setPalvelupisteID(int ID) {
+		this.palvelupisteID = ID;
+	}
+
+	public int getPalvelupisteID() {
+		return palvelupisteID;
+	}
+
 	public void setTyyppi(TapahtumanTyyppi tyyppi) {
 		this.tyyppi = tyyppi;
 	}
+
 	public TapahtumanTyyppi getTyyppi() {
 		return tyyppi;
 	}
+
 	public void setAika(double aika) {
 		this.aika = aika;
 	}
+
 	public double getAika() {
 		return aika;
 	}
 
 	@Override
 	public int compareTo(Tapahtuma arg) {
-		if (this.aika < arg.aika) return -1;
-		else if (this.aika > arg.aika) return 1;
+		if (this.aika < arg.aika)
+			return -1;
+		else if (this.aika > arg.aika)
+			return 1;
 		return 0;
 	}
 }
