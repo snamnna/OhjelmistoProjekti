ae949ededcca37ad0da1ef04de094496e7f73edd
matiasdn <matiasdn@metropolia.fi>
2023-02-18 11:23:37 +0200

    access modifier fix

--- src/simu/framework/IMoottori.java
+++ src/simu/framework/IMoottori.java
@@ -3,9 +3,9 @@ package simu.framework;
 public interface IMoottori {
 	// Kontrolleri käyttää tätä rajapintaa
 	public void setSimulointiaika(double aika);
-	protected void setLabJakauma(); //asetettava jakauma
+	public void setLabJakauma(); //asetettava jakauma
 	public void setYlaakarienLkm(); //asetettavat palvelupisteet
 	public void setElaakarienLkm();
-	//tarvitaanko muita?
 	public void setViive(long viive);
+	public long getViive();
 }
