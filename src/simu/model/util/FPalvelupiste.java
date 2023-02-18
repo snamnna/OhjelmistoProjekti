ff813adea22dced78299febe8c67210c21c2b0b7
matiasdn <matiasdn@metropolia.fi>
2023-02-18 21:53:25 +0200

    formatting

--- src/simu/model/util/FPalvelupiste.java
+++ src/simu/model/util/FPalvelupiste.java
@@ -5,19 +5,17 @@ import simu.framework.Tapahtumalista;
 import simu.model.*;
 
 public class FPalvelupiste {
-    public static IPalvelupiste createPalvelupiste(TapahtumanTyyppi tyyppi, Tapahtumalista tapahtumalista) {
-        IPalvelupiste uusiPalvelupiste;
-        switch (tyyppi) {
-            case ARR: uusiPalvelupiste = new Sairaanhoitaja(new Normal(10, 6), tapahtumalista, TapahtumanTyyppi.ARR);
-                break;
-            case YLARR: uusiPalvelupiste = new YLaakari(new Normal(10, 10), tapahtumalista, TapahtumanTyyppi.YLARR);
-                break;
-            case ELARR: uusiPalvelupiste = new ELaakari(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.ELARR);
-                break;
-            case LABRA_ARRIVAL: uusiPalvelupiste = new Labra(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.LABRA_ARRIVAL);
-                break;
-            default: throw new IllegalArgumentException("Unexpected value: " + tyyppi);
-        };
-        return uusiPalvelupiste;
-    }
+	public static IPalvelupiste createPalvelupiste(TapahtumanTyyppi tyyppi, Tapahtumalista tapahtumalista) {
+		IPalvelupiste uusiPalvelupiste;
+		switch (tyyppi) {
+		case ARR -> uusiPalvelupiste = new Sairaanhoitaja(new Normal(10, 6), tapahtumalista, TapahtumanTyyppi.ARR);
+		case YLARR -> uusiPalvelupiste = new YLaakari(new Normal(10, 10), tapahtumalista, TapahtumanTyyppi.YLARR);
+		case ELARR -> uusiPalvelupiste = new ELaakari(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.ELARR);
+		case LABRA_ARRIVAL ->
+			uusiPalvelupiste = new Labra(new Normal(5, 3), tapahtumalista, TapahtumanTyyppi.LABRA_ARRIVAL);
+		default -> throw new IllegalArgumentException("Unexpected value: " + tyyppi);
+		}
+		;
+		return uusiPalvelupiste;
+	}
 }
