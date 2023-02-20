1eb746b2eb65dbe32eb26e03f87b7cd91f17fae0
matiasdn <matiasdn@metropolia.fi>
2023-02-20 15:13:51 +0200

    lisätty testi-konstruktori, kommentteja ja fixattu koodi

--- src/simu/model/OmaMoottori.java
+++ src/simu/model/OmaMoottori.java
@@ -8,31 +8,86 @@ import simu.framework.Tapahtuma;
 import simu.model.util.FPalvelupiste;
 import simu.model.util.IPalvelupiste;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
 import controller.IKontrolleriVtoM;
 
 public class OmaMoottori extends Moottori {
 
 	private final Saapumisprosessi saapumisprosessi;
+	private Map<TapahtumanTyyppi, List<IPalvelupiste>> tyyppiToPalveluPMap;
+	private Boolean test = false;
 
 	public OmaMoottori(IKontrolleriVtoM kontrolleri) {
 		super(kontrolleri);
 		saapumisprosessi = new Saapumisprosessi(new Negexp(15, 5), tapahtumalista, TapahtumanTyyppi.ARR);
+		tyyppiToPalveluPMap = new HashMap<>();
+	}
+
+	public OmaMoottori(IKontrolleriVtoM kontrolleri, Boolean testi) {
+		super(kontrolleri);
+		saapumisprosessi = new Saapumisprosessi(new Negexp(15, 5), tapahtumalista, TapahtumanTyyppi.ARR);
+		tyyppiToPalveluPMap = new HashMap<>();
+		this.test = testi;
 	}
 
 	@Override
 	protected void alustukset() {
-		Map<String, Integer> pPisteet = kontrolleri.haePalvelupisteet();
+		// key on palvelupisteen tyyppi.
+		// arvona tietyn tyyppisten palvelupisteiden lukumäärä
+		Map<String, Integer> pPisteet = test ? kontrolleri.haePalvelupisteetTest() : kontrolleri.haePalvelupisteet();
 
-		for (String s : pPisteet.keySet()) {
-			// palvelupisteiden alustus
-			for (int i = 0; i < pPisteet.get(s); i++) {
-				IPalvelupiste palvelupiste = FPalvelupiste.createPalvelupiste(getTapahtumanTyyppi(s), tapahtumalista);
+		// palvelupisteiden alustus
+		for (String key : pPisteet.keySet()) {
+			TapahtumanTyyppi tArrival = getTapahtumanTyyppi(key);
+			TapahtumanTyyppi tDeparture = getTapahtumanTyyppi(key, false);
+			tyyppiToPalveluPMap.putIfAbsent(tArrival, new ArrayList<>());
+			// departure viittaa samaan listaan ku sitä vastaava arrival, esim YLDEP ja
+			// YLARR
+			tyyppiToPalveluPMap.putIfAbsent(tDeparture, tyyppiToPalveluPMap.get(tArrival));
+
+			// alustetaan tyypin palvelupisteet
+			for (int i = 0; i < pPisteet.get(key); i++) {
+				IPalvelupiste palvelupiste = FPalvelupiste.createPalvelupiste(tArrival, tapahtumalista);
 				palvelupisteet.putIfAbsent(palvelupiste.getID(), palvelupiste);
+
+				// ei tarvitse lisätä myös tyyppiToPalveluPMap, koska se "osoittaa" jo valmiiksi
+				// samaan listaan
+				tyyppiToPalveluPMap.get(tArrival).add(palvelupiste);
 			}
 		}
 
 		saapumisprosessi.generoiSeuraava(); // Ensimmäinen saapuminen järjestelmään
+	}
+
+	@Override
+	protected void suoritaTapahtuma(Tapahtuma t) { // B-vaiheen tapahtumat
+		TapahtumanTyyppi tyyppi = t.getTyyppi();
+		for (IPalvelupiste p : palvelupisteet.values()) {
+			System.out.println(p.getJonoString());
+		}
+		List<IPalvelupiste> pisteet = tyyppiToPalveluPMap.get(tyyppi);
+
+		// tällä hetkellä hakee randomisti vaan jonku tapahtumatyyppiä vastaavan
+		// palvelupisteen
+		// pitää lisätä switch() jos halutaan erilaisia kriteerejä miten se palvelupiste
+		// valitaan eri tyypeille
+		// jos halutaan vaan että hakee palvelupisteen jolla on vähiten jonoa tai on
+		// vapaa niin implementoidaan se tähän tilalle
+		int rndIndex = ThreadLocalRandom.current().nextInt(0, pisteet.size());
+		pisteet.get(rndIndex).siirraAsiakas(t, palvelupisteet);
+
+		// jos asiakas saapuu ekaa kertaa simulaattoriin eli sen tapahtumatyyppi on ARR
+		// generoidaan seuraava saapuminen ja kutsutaan kontrollerin metodia, joka
+		// hoitaa visualisoinnin
+		if (onArrival(tyyppi)) {
+			saapumisprosessi.generoiSeuraava();
+			kontrolleri.visualisoiAsiakas();
+		}
 	}
 
 	protected TapahtumanTyyppi getTapahtumanTyyppi(String tyyppi, Boolean arrival) {
@@ -50,12 +105,8 @@ public class OmaMoottori extends Moottori {
 		return getTapahtumanTyyppi(tyyppi, true);
 	}
 
-	@Override
-	protected void suoritaTapahtuma(Tapahtuma t) { // B-vaiheen tapahtumat
-		TapahtumanTyyppi tyyppi = t.getTyyppi();
-		palvelupisteet.get(t.getPalvelupisteID()).siirraAsiakas(t, palvelupisteet);
-		if (tyyppi == TapahtumanTyyppi.ARR)
-			saapumisprosessi.generoiSeuraava();
+	private Boolean onArrival(TapahtumanTyyppi tyyppi) {
+		return tyyppi == TapahtumanTyyppi.ARR;
 	}
 
 	@Override
