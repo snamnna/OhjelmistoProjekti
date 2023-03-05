99e175adfd762a4c2f84641a9f69839a0937ce61
edvni <edvard.nivala@metropolia.fi>
2023-03-05 19:44:00 +0200

    getAllTulokset() metodi
    Ei testattu metodi, jolla voisi hakea tietokannasta kaikki Tulokset ja esittää ne Tietovarasto ikkunan TableViewssä. Work in progress..

--- src/simulaattori/simu/model/TulosDAO.java
+++ src/simulaattori/simu/model/TulosDAO.java
@@ -1,7 +1,13 @@
 package simulaattori.simu.model;
 
+import java.util.List;
+
+import datasource.MAriaDBConnector;
 import entity.Tulos;
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class TulosDAO {
 
@@ -22,4 +28,32 @@ public class TulosDAO {
 		em.getTransaction().commit();
 		System.out.println("Tulokset viety");
 	}
+	
+	/*
+	 * Using Criteria API to construct a query to fetch all Tulos objects from the database table.
+	 * The query is executed by calling the getResultList() method on the created query object.
+	 * The fetched Tulos objects are returned as a list.
+	 */
+	
+	public static List<Tulos> getAllTulokset() {
+        List<Tulos> tulosList = null;
+        try {
+            EntityManager entityManager = datasource.MAriaDBConnector.getInstance();
+            entityManager.getTransaction().begin();
+
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Tulos> criteriaQuery = criteriaBuilder.createQuery(Tulos.class);
+            Root<Tulos> root = criteriaQuery.from(Tulos.class);
+            criteriaQuery.select(root);
+
+            tulosList = entityManager.createQuery(criteriaQuery).getResultList();
+
+            entityManager.getTransaction().commit();
+            entityManager.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tulosList;
+    }
 }
