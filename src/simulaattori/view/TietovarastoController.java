19cab9fcec3f495383bbccc373b836d1a819ecc6
edvni <edvard.nivala@metropolia.fi>
2023-03-06 18:46:31 +0200

    TietovarastoController update
    setMainapp etc

--- src/simulaattori/view/TietovarastoController.java
+++ src/simulaattori/view/TietovarastoController.java
@@ -1,6 +1,11 @@
 package simulaattori.view;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
+
+import org.hibernate.Session;
+
 import entity.Tulos;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,7 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
+import simulaattori.MainApp;
 import simulaattori.simu.model.TulosDAO;
 
 public class TietovarastoController {
@@ -38,14 +43,29 @@ public class TietovarastoController {
     @FXML
     private TableColumn<Tulos, Double> col_serviceTime;
     
-    private Stage dialogStage;
+    private MainApp mainApp;
     
-    ObservableList<Tulos> listTulos = FXCollections.observableArrayList();;
+    ObservableList<Tulos> listTulos = FXCollections.observableArrayList();
     
     @FXML
+    public void setMainApp(MainApp mainApp) {
+    	this.mainApp = mainApp;
+    }
+    
+    /*
+    @FXML
 	public void initialize() {
-    	col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        col_aika.setCellValueFactory(new PropertyValueFactory<>("simTime"));
+    	col_id.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getId()));
+        col_aika.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getSimTime()));
+        col_eLaakarit.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getErikoislaakarit()));
+        col_yleisLaakarit.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getYleislaakarit()));
+        col_labrakaynnit.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getLabraArrivalit()));
+        col_saapuneet.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getArrivalCount()));
+        col_palveltu.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getCompletedCount()));
+        col_busyTime.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getBusyTime()));
+        col_throughput.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getThroughput()));
+        col_utilization.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getUtilization()));
+        col_serviceTime.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getServiceTime()));
 
         //  fetch the Tulos objects
         List<Tulos> tulosList = TulosDAO.getAllTulokset();
@@ -56,8 +76,5 @@ public class TietovarastoController {
         //  set it as the items of the tableview using the setItems() method.
         table_tulokset.setItems(tulosObservableList);
     }
-    
-    public void setDialogStage(Stage dialogStage) {
-    	this.dialogStage = dialogStage;
-    }
+    */
 }
