8947bba823b9d998ddbb859edc3432a93235da58
Matias <matiasdn@metropolia.fi>
2023-03-07 16:02:52 +0200

    formatting

--- src/simulaattori/view/SimulaattoriController.java
+++ src/simulaattori/view/SimulaattoriController.java
@@ -11,6 +11,10 @@ public class SimulaattoriController {
 
     private MainApp mainApp;
     private GraphicsContext gc;
+    private int sairaanhoitajaLkm;
+    private int yLaakariLkm;
+    private int eLaakariLkm;
+    private int labraLkm;
 
     double i = 0;
     double j = 10;
@@ -18,32 +22,39 @@ public class SimulaattoriController {
     @FXML
     private AnchorPane parentPane;
     @FXML
-    private Canvas visualisointiCanvas;
+    private Canvas canvas;
 
     @FXML
     private void initialize() {
-        gc = visualisointiCanvas.getGraphicsContext2D();
-        AnchorPane.setTopAnchor(visualisointiCanvas, 0.0);
-        AnchorPane.setRightAnchor(visualisointiCanvas, 0.0);
-        AnchorPane.setBottomAnchor(visualisointiCanvas, 0.0);
-        AnchorPane.setLeftAnchor(visualisointiCanvas, 0.0);
-        visualisointiCanvas.widthProperty().bind(parentPane.widthProperty());
-        visualisointiCanvas.heightProperty().bind(parentPane.heightProperty());
+        gc = canvas.getGraphicsContext2D();
+        AnchorPane.setTopAnchor(canvas, 0.0);
+        AnchorPane.setRightAnchor(canvas, 0.0);
+        AnchorPane.setBottomAnchor(canvas, 0.0);
+        AnchorPane.setLeftAnchor(canvas, 0.0);
+        canvas.widthProperty().bind(parentPane.widthProperty());
+        canvas.heightProperty().bind(parentPane.heightProperty());
     }
 
     public void tyhjennaNaytto() {
         gc.setFill(Color.rgb(18,18,18));
-        gc.fillRect(0, 0, visualisointiCanvas.getWidth(), visualisointiCanvas.getHeight());
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
     public void uusiAsiakas() {
         gc.setFill(Color.RED);
         gc.fillOval(i,j,10,10);
-        i = (i + 10) % visualisointiCanvas.getWidth();
-        if (i==0) j+=10;
+        i += 10;
+        if (i >= canvas.getWidth()) {
+            i = 0;
+            j += 10;
+        }
     }
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
+
+	public void alustukset() {
+		tyhjennaNaytto();
+	}
 }
