d4ce1d094bbaaec9bf955f0e3a352ddac3c63880
Matias <matiasdn@metropolia.fi>
2023-03-07 19:20:40 +0200

    poistettu ylimääräiset metodit ja lisätty getTulos

--- src/simulaattori/simu/framework/IMoottori.java
+++ src/simulaattori/simu/framework/IMoottori.java
@@ -1,11 +1,11 @@
 package simulaattori.simu.framework;
 
+import entity.Tulos;
+
 public interface IMoottori {
 	// Kontrolleri käyttää tätä rajapintaa
-	public void setSimulointiaika(double aika);
-	public void setLabJakauma(); //asetettava jakauma
-	public void setYlaakarienLkm(); //asetettavat palvelupisteet
-	public void setElaakarienLkm();
-	public void setViive(long viive);
-	public long getViive();
+	void setSimulointiaika(double aika);
+	void setViive(long viive);
+	long getViive();
+	Tulos getTulos();
 }
