6fb75a2ec6f629d03dd6968c6b687a5d9a644dc9
Matias <matiasdn@metropolia.fi>
2023-03-08 18:07:34 +0200

    implementoitu jonojen visualisointi

--- src/simulaattori/view/SimulaattoriController.java
+++ src/simulaattori/view/SimulaattoriController.java
@@ -1,60 +1,129 @@
 package simulaattori.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import simulaattori.MainApp;
+import simulaattori.simu.model.TapahtumanTyyppi;
+import simulaattori.simu.model.util.IPalvelupiste;
 
 public class SimulaattoriController {
 
-    private MainApp mainApp;
-    private GraphicsContext gc;
-    private int sairaanhoitajaLkm;
-    private int yLaakariLkm;
-    private int eLaakariLkm;
-    private int labraLkm;
+	private MainApp mainApp;
+	private GraphicsContext gc;
+	private Map<TapahtumanTyyppi, List<IPalvelupiste>> palvelupisteet;
+	private List<TapahtumanTyyppi> tyypit;
 
-    double i = 0;
-    double j = 10;
+	@FXML
+	private AnchorPane parentPane;
+	@FXML
+	private Canvas canvas;
 
-    @FXML
-    private AnchorPane parentPane;
-    @FXML
-    private Canvas canvas;
-
-    @FXML
-    private void initialize() {
-        gc = canvas.getGraphicsContext2D();
-        AnchorPane.setTopAnchor(canvas, 0.0);
-        AnchorPane.setRightAnchor(canvas, 0.0);
-        AnchorPane.setBottomAnchor(canvas, 0.0);
-        AnchorPane.setLeftAnchor(canvas, 0.0);
-        canvas.widthProperty().bind(parentPane.widthProperty());
-        canvas.heightProperty().bind(parentPane.heightProperty());
-    }
-
-    public void tyhjennaNaytto() {
-        gc.setFill(Color.rgb(18,18,18));
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-    }
-
-    public void uusiAsiakas() {
-        gc.setFill(Color.RED);
-        gc.fillOval(i,j,10,10);
-        i += 10;
-        if (i >= canvas.getWidth()) {
-            i = 0;
-            j += 10;
-        }
-    }
-
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
-    }
-
-	public void alustukset() {
-		tyhjennaNaytto();
+	@FXML
+	private void initialize() {
+		gc = canvas.getGraphicsContext2D();
+		tyypit = Arrays.asList(TapahtumanTyyppi.ARR, TapahtumanTyyppi.YLARR,
+				TapahtumanTyyppi.ELARR, TapahtumanTyyppi.LABRA_ARRIVAL);
 	}
+
+	public void tyhjennaNaytto() {
+		gc.setFill(Color.rgb(18, 18, 18));
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	}
+
+	public void visualisoi() {
+		// visualisoidaan palvelupisteet bufferi canvakseen ensin
+		// jotta käyttöliittymän canvakseen ei tule keskeneräisiä visualisointeja
+		Canvas buffer = new Canvas(canvas.getWidth(), canvas.getHeight());
+		GraphicsContext bufferGC = buffer.getGraphicsContext2D();
+
+		bufferGC.setFill(Color.rgb(18, 18, 18));
+		bufferGC.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		bufferGC.setFill(Color.RED);
+
+		int palvelupisteCount = 0;
+		for (int i = 0; i < tyypit.size(); i++) {
+			palvelupisteCount += palvelupisteet.get(tyypit.get(i)).size();
+		}
+
+		double width = canvas.getWidth() / palvelupisteCount;
+		int count = 0;
+		for (TapahtumanTyyppi tyyppi : tyypit) {
+			List<IPalvelupiste> palvelupisteLista = palvelupisteet.get(tyyppi);
+			if (palvelupisteLista == null) {
+				continue;
+			}
+
+			for (IPalvelupiste palvelupiste : palvelupisteLista) {
+				double x = count * width;
+				double y = 20;
+
+				String jononTyyppi = palvelupiste.getSkeduloitavanTapahtumanTyyppi().toString();
+				bufferGC.setFill(Color.rgb(128, 128, 128));
+				bufferGC.setTextAlign(TextAlignment.CENTER);
+				bufferGC.fillText(jononNimi(jononTyyppi), x + width / 2, y - 10);
+				bufferGC.setFill(getJononVari(jononTyyppi));
+
+				for (int i = 0; i < palvelupiste.getAsiakasMaaraJonossa(); i++) {
+					bufferGC.fillOval(x + width / 2 - 5, y, 10, 10);
+					y += 20;
+					if (y >= canvas.getHeight()) {
+						y = 20;
+					}
+				}
+				y += 20;
+				count++;
+			}
+		}
+
+		// piirretään bufferoitu kuva käyttöliittymään
+		gc.drawImage(buffer.snapshot(null, null), 0, 0);
+	}
+
+	private String jononNimi(String tyyppi) {
+		switch (tyyppi.toString()) {
+		case "ARR":
+			return "Sairaanhoitaja";
+		case "YLARR":
+			return "Yleislääkäri";
+		case "ELARR":
+			return "Erikoislääkäri";
+		case "LABRA_ARRIVAL":
+			return "Laboratorio";
+		default:
+			return "?";
+		}
+	}
+
+	private static Color getJononVari(String tyyppi) {
+		switch (tyyppi) {
+		case "ARR":
+			return Color.GHOSTWHITE;
+		case "YLARR":
+			return Color.CYAN;
+		case "ELARR":
+			return Color.DARKCYAN;
+		case "LABRA_ARRIVAL":
+			return Color.YELLOW;
+		default:
+			return Color.WHITE;
+		}
+	}
+
+	public void alustukset(Map<TapahtumanTyyppi, List<IPalvelupiste>> palvelupisteet) {
+		tyhjennaNaytto();
+		this.palvelupisteet = palvelupisteet;
+	}
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+
 }
