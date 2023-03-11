f68510ee98271ee1b1ea4c6c33caf33f6562c18c
Matias <matiasdn@metropolia.fi>
2023-03-11 21:49:50 +0200

    poistettu koodia jota ei käytetty

--- src/simulaattori/simu/model/Asiakas.java
+++ src/simulaattori/simu/model/Asiakas.java
@@ -4,86 +4,69 @@ import simulaattori.simu.framework.Kello;
 import simulaattori.simu.framework.Trace;
 
 
-// TODO:
-// Asiakas koodataan simulointimallin edellyttämällä tavalla (data!)
 public class Asiakas {
-	private double saapumisaika;
-	private double poistumisaika;
-	private final int id;
-	private static double waitingTime;
-	private static int i = 1;
-	private static double sum = 0;
-	private boolean labrassaKayty = false;
-	//tallennetaan viimeisin annettu id tuloksia varten
-	private static int viimeisinID;
-
-	private static int asiakasCount = 0;
-
-	private static int completed;
-	private static double totalWaitingTime;
+    private static int i = 1;
+    private static double sum = 0;
+    private static int asiakasCount = 0;
+    private final int id;
+    private double saapumisaika;
+    private double poistumisaika;
+    private boolean labrassaKayty = false;
 
 
-	public Asiakas() {
-		id = i++;
-		//viimeisin id sama arvo, kuin id, eli i
-		viimeisinID = i;
-		asiakasCount++;
+    public Asiakas() {
+        id = i++;
+        asiakasCount++;
 
-		saapumisaika = Kello.getInstance().getAika();
-		Trace.out(Trace.Level.INFO, "Uusi asiakas nro " + id + " saapui klo " + saapumisaika);
-	}
-	
-	public static int getAsiakasCount() {
-		return asiakasCount;
-	}
+        saapumisaika = Kello.getInstance().getAika();
+        Trace.out(Trace.Level.INFO, "Uusi asiakas nro " + id + " saapui klo " + saapumisaika);
+    }
 
-	public static void reset() {
-		asiakasCount = 0;
-		sum = 0;
-	}
+    public static void reset() {
+        asiakasCount = 0;
+        sum = 0;
+    }
 
-	public double getPoistumisaika() {
-		return poistumisaika;
-	}
+    public static int getAsiakasCount() {
+        return asiakasCount;
+    }
 
-	public void setPoistumisaika(double poistumisaika) {
-		this.poistumisaika = poistumisaika;
-	}
+    public static double getTotalWaitingTime() {
+        return sum;
+    }
 
-	public double getSaapumisaika() {
-		return saapumisaika;
-	}
+    public double getPoistumisaika() {
+        return poistumisaika;
+    }
 
-	public void setSaapumisaika(double saapumisaika) {
-		this.saapumisaika = saapumisaika;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setLabrakaynti(boolean kayty) {
-		this.labrassaKayty = kayty;
-	}
-	public boolean getLabrakaynti() {
-		return labrassaKayty;
-	}
-	
-	public static double getTotalWaitingTime() {
-		return sum;
-	}
+    public void setPoistumisaika(double poistumisaika) {
+        this.poistumisaika = poistumisaika;
+    }
 
-	public static int getViimeisinID() {
-		return viimeisinID;
-	}
+    public double getSaapumisaika() {
+        return saapumisaika;
+    }
 
-	public static double getWaitingTime() {
-		return waitingTime;
-	}
+    public void setSaapumisaika(double saapumisaika) {
+        this.saapumisaika = saapumisaika;
+    }
 
-	public void raportti() {
-		sum += (poistumisaika - saapumisaika);
-//		double keskiarvo = sum / asiakasCount;
-//		waitingTime = keskiarvo;
-		//Trace.out(Trace.Level.INFO,"Asiakkaiden läpimenoaikojen keskiarvo tähän asti "+ keskiarvo);
-	}
+    public int getId() {
+        return id;
+    }
+
+    public boolean getLabrakaynti() {
+        return labrassaKayty;
+    }
+
+    public void setLabrakaynti(boolean kayty) {
+        this.labrassaKayty = kayty;
+    }
+
+    public void raportti() {
+        sum += (poistumisaika - saapumisaika);
+        System.out.println(Labra.departures);
+        double keskiarvo = sum / Labra.departures;
+        //Trace.out(Trace.Level.INFO,"Asiakkaiden läpimenoaikojen keskiarvo tähän asti "+ keskiarvo);
+    }
 }
