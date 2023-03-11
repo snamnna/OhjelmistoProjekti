7b9b9cb40e79c1469ce1df121e83a63665fc8345
Matias <matiasdn@metropolia.fi>
2023-03-11 21:33:50 +0200

    palvelupisteiden määrät tuodaan käyttäen niiden tyyppiä

--- src/simulaattori/controller/Kontrolleri.java
+++ src/simulaattori/controller/Kontrolleri.java
@@ -1,102 +1,97 @@
 package simulaattori.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import entity.Tulos;
 import javafx.application.Platform;
 import simulaattori.simu.framework.IMoottori;
-import simulaattori.simu.framework.Moottori;
 import simulaattori.simu.model.OmaMoottori;
 import simulaattori.simu.model.TapahtumanTyyppi;
 import simulaattori.simu.model.util.IPalvelupiste;
 import simulaattori.view.ISimulaattorinUI;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Kontrolleri implements IKontrolleriVtoM, IKontrolleriMtoV {
 
-	private ISimulaattorinUI ui;
-	private IMoottori moottori;
-	private volatile boolean simulointiKaynnissa;
+    private ISimulaattorinUI ui;
+    private IMoottori moottori;
+    private volatile boolean simulointiKaynnissa;
 
-	public Kontrolleri(ISimulaattorinUI ui) {
-		this.ui = ui;
-		simulointiKaynnissa = false;
-	}
+    public Kontrolleri(ISimulaattorinUI ui) {
+        this.ui = ui;
+        simulointiKaynnissa = false;
+    }
 
-	public void visualisoi() {
-		Platform.runLater(() -> ui.getVisualisointi().visualisoi());
-	}
+    public void visualisoi() {
+        Platform.runLater(() -> ui.getVisualisointi().visualisoi());
+    }
 
-	@Override
-	public void kaynnistaSimulointi() {
-		if (!simulointiKaynnissa) {
-			moottori = new OmaMoottori(this); // luodaan uusi moottorisäie jokaista simulointia varten
-			moottori.setSimulointiaika(ui.getSimulointiAika());
-			moottori.setViive(ui.getViive());
-			((Thread) moottori).start();
-			simulointiKaynnissa = true;
-		}
-	}
+    @Override
+    public void kaynnistaSimulointi() {
+        if (!simulointiKaynnissa) {
+            moottori = new OmaMoottori(this); // luodaan uusi moottorisäie jokaista simulointia varten
+            moottori.setSimulointiaika(ui.getSimulointiAika());
+            moottori.setViive(ui.getViive());
+            ((Thread) moottori).start();
+            simulointiKaynnissa = true;
+        }
+    }
 
-	public void simulointiPaattyi() {
-		Platform.runLater(() -> {
-			ui.setSimuloidaan(false);
-			simulointiKaynnissa = false;
-		});
-	}
+    public void simulointiPaattyi() {
+        Platform.runLater(() -> {
+            ui.setSimuloidaan(false);
+            simulointiKaynnissa = false;
+        });
+    }
 
-	@Override
-	public int haeYlaakarienLkm() {
-		return ui.getYlaakarienLkm();
-	}
+    @Override
+    public int haeYlaakarienLkm() {
+        return ui.getYlaakarienLkm();
+    }
 
-	@Override
-	public int haeElaakarienLkm() {
-		return ui.getElaakarienLkm();
-	}
+    @Override
+    public int haeElaakarienLkm() {
+        return ui.getElaakarienLkm();
+    }
 
-	@Override
-	public int haeSairaanhoitajaLkm() {
-		return ui.getSairaanhoitajaLkm();
-	}
+    @Override
+    public int haeSairaanhoitajaLkm() {
+        return ui.getSairaanhoitajaLkm();
+    }
 
-	@Override
-	public int haeLabraLkm() {
-		return ui.getLabraLkm();
-	}
+    @Override
+    public int haeLabraLkm() {
+        return ui.getLabraLkm();
+    }
 
-	@Override
-	public Map<String, Integer> haePalvelupisteet() {
-		Map<String, Integer> palvelupisteet = new HashMap<>();
-		palvelupisteet.put("Sairaanhoitaja", haeSairaanhoitajaLkm());
-		palvelupisteet.put("YLaakari", haeYlaakarienLkm());
-		palvelupisteet.put("ELaakari", haeElaakarienLkm());
-		palvelupisteet.put("Labra", haeLabraLkm());
-		return palvelupisteet;
-	}
+    @Override
+    public Map<TapahtumanTyyppi, Integer> haePalvelupisteet() {
+        Map<TapahtumanTyyppi, Integer> palvelupisteet = new HashMap<>();
+        palvelupisteet.put(TapahtumanTyyppi.ARR, haeSairaanhoitajaLkm());
+        palvelupisteet.put(TapahtumanTyyppi.YLARR, haeYlaakarienLkm());
+        palvelupisteet.put(TapahtumanTyyppi.ELARR, haeElaakarienLkm());
+        palvelupisteet.put(TapahtumanTyyppi.LABRA_ARRIVAL, haeLabraLkm());
+        return palvelupisteet;
+    }
 
-	@Override
-	public void hidasta() {
-		moottori.setViive((long) (moottori.getViive() * 1.10));
-	}
+    @Override
+    public void hidasta() {
+        moottori.setViive((long) (moottori.getViive() * 1.10));
+    }
 
-	@Override
-	public void nopeuta() {
-		moottori.setViive((long) (moottori.getViive() * 0.9));
-	}
+    @Override
+    public void nopeuta() {
+        moottori.setViive((long) (moottori.getViive() * 0.9));
+    }
 
-	public Moottori getMoottori() {
-		return (Moottori) moottori;
-	}
+    @Override
+    public Tulos getTulos() {
+        return moottori.getTulos();
+    }
 
-	@Override
-	public Tulos getTulos() {
-		return moottori.getTulos();
-	}
-
-	@Override
-	public void setPalvelupisteet(Map<TapahtumanTyyppi, List<IPalvelupiste>> palvelupisteet) {
-		ui.setPalvelupisteet(palvelupisteet);
-	}
+    @Override
+    public void setPalvelupisteet(Map<TapahtumanTyyppi, List<IPalvelupiste>> palvelupisteet) {
+        ui.setPalvelupisteet(palvelupisteet);
+    }
 }
